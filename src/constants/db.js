dde1d0250611344e027044549301e4cc350f9ac6
Пользователь <Makhammadov usmonjon>
2023-10-24 21:55:53 +0500

    first commit

---
+++ src/constants/db.js
@@ -0,0 +1,60 @@
+export function getData() {
+	return [
+		{
+			title: 'TikTok',
+			price: 1,
+			Image:
+				'https://www.like4like.org/img/blog/tiktok-followers.png',
+			id: 1,
+		},
+		{
+			title: 'Instagram',
+			price: 0.7,
+			Image:
+				'https://s.inalmaty.kz/section/promonewsintext/upload/images/promo/intext/000/055/295/inst-l_6437e8b0b610b.jpg',
+			id: 2,
+		},
+		{
+			title: 'YouTube',
+			price: 3,
+			Image:
+				'https://fireseo.ru/wp-content/uploads/2020/06/nakrutka-prosmotrov-youtube-4.jpg',
+			id: 3,
+		},
+		{
+			title: 'Vk (like)',
+			price: 0.6,
+			Image:
+				'https://is1-ssl.mzstatic.com/image/thumb/Purple49/v4/b1/9c/5f/b19c5f77-4701-36e8-702f-2541b08e9142/source/512x512bb.jpg',
+			id: 4,
+		},
+		{
+			title: 'Telegram',
+			price: 1.2,
+			Image:
+				'https://dimox.name/wp-content/uploads/2022/05/nakrutit-podpischikov-telegram-deshevo.jpg',
+			id: 5,
+		},
+		{
+			title: 'Facebook',
+			price: 2,
+			Image:
+				'https://www.smm-marketing.com.ua/image/cache/catalog/product/facebook-228x228.png',
+			id: 6,
+		},
+		{
+			title: 'Zoom (otziv)',
+			price: 2.5,
+			Image:
+				'https://softolet.ru/wp-content/uploads/2020/06/Screenshot_351.png',
+			id: 7,
+		},
+		{
+			title: 'Live',
+			price: 3.5,
+			Image:
+				'https://postium.ru/wp-content/uploads/2023/01/obzor-servisa-nakrutka-zriteley-1.jpg',
+			id: 8,
+		},
+	];
+}
